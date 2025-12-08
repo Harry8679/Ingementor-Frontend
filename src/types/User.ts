f6036d0400c5cf4dc4965e1a4a362d5fe273b3fa
Harry7986 <emarh.harry.code@gmail.com>
2025-12-08 09:30:51 +0100

    Login 2

--- src/types/User.ts
+++ src/types/User.ts
@@ -2,19 +2,24 @@ export interface User {
   id: number;
   email: string;
   userType: 'teacher' | 'student' | 'parent' | 'admin' | 'super_admin';
+
+  // Champs souvent présents mais parfois optionnels selon l’API
   firstName?: string;
   lastName?: string;
   fullName?: string;
   phone?: string | null;
   roles?: string[];
+
   bio?: string;
   experience?: string;
   isVerified?: boolean;
   rating?: string;
+
   grade?: {
     id: number;
     name: string;
     level: string;
   };
+
   address?: string;
 }
