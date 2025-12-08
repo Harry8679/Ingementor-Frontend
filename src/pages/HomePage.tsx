d46506e9cd945ef61a980d873fd343ee74015eae
Harry7986 <emarh.harry.code@gmail.com>
2025-12-08 11:17:27 +0100

    Home Page 2

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,10 +2,8 @@ import { Link } from 'react-router-dom';
 import { 
   AcademicCapIcon, 
   UserGroupIcon, 
-  SparklesIcon,
   CheckCircleIcon,
   ArrowRightIcon,
-  ClockIcon,
   CurrencyEuroIcon,
   ShieldCheckIcon,
   ChatBubbleLeftRightIcon,
