976894fa5d9042cbef86b5564f9fc56e92f1964d
Harry7986 <emarh.harry.code@gmail.com>
2025-12-10 14:31:47 +0100

    Subject 2

--- src/pages/teacher/Subjects.tsx
+++ src/pages/teacher/Subjects.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-
 import Navbar from "../../components/layout/Navbar";
 import Sidebar from "../../components/layout/Sidebar";
 import Card from "../../components/common/Card";
@@ -13,7 +11,6 @@ import type { Subject } from "../../types/common.types";
 const Subjects: React.FC = () => {
   const [loading, setLoading] = useState(true);
   const [subjects, setSubjects] = useState<Subject[]>([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     loadSubjects();
@@ -32,14 +29,14 @@ const Subjects: React.FC = () => {
 
   if (loading) {
     return (
-      <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 flex items-center justify-center">
+      <div className="min-h-screen bg-linear-to-br from-slate-50 via-blue-50 to-indigo-50 flex items-center justify-center">
         <Spinner size="lg" />
       </div>
     );
   }
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50">
+    <div className="min-h-screen bg-linear-to-br from-slate-50 via-blue-50 to-indigo-50">
 
       {/* Décor animé */}
       <div className="absolute top-0 left-0 w-96 h-96 bg-purple-300 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob" />
