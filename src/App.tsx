f685f1ba5f129484112faed935850ca8d51c60b9
Harry7986 <emarh.harry.code@gmail.com>
2026-02-06 13:24:04 +0100

    App 6

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,8 @@ import StudentProgress from './pages/student/Student_Progress';
 import StudentLessons from './pages/student/Student_Lessons';
 import StudentMessages from './pages/student/Student_Messages';
 import StudentSettings from './pages/student/Student_Settings';
+
+// Teacher Pages
 import TeacherProfile from './pages/teacher/Teacher_Profile';
 import TeacherStudents from './pages/teacher/TeacherStudents';
 import TeacherSubjects from './pages/teacher/Teacher_Subject';
@@ -23,6 +25,15 @@ import TeacherAvailability from './pages/teacher/Teacher_Availability';
 import TeacherMessages from './pages/teacher/Teacher_Messages';
 import TeacherStatistics from './pages/teacher/Teacher_Statistics';
 import TeacherSettings from './pages/teacher/Teacher_Settings';
+
+// Parent Pages
+import ParentChildren from './pages/parent/Parent_Children';
+import ParentGrades from './pages/parent/Parent_Grades';
+import ParentLessons from './pages/parent/Parent_Lessons';
+import ParentTeachers from './pages/parent/Parent_Teachers';
+import ParentMessages from './pages/parent/Parent_Messages';
+import ParentPayments from './pages/parent/Parent_Payments';
+import ParentSettings from './pages/parent/Parent_Settings';
 
 function App() {
   return (
@@ -34,9 +45,9 @@ function App() {
         <Route path="/inscription" element={<RegisterPage />} />
         
         {/* Dashboard Routes */}
-        <Route path="/dashboard/teacher/*" element={<TeacherDashboard />} />
+        <Route path="/dashboard/teacher" element={<TeacherDashboard />} />
         <Route path="/dashboard/student" element={<StudentDashboard />} />
-        <Route path="/dashboard/parent/*" element={<ParentDashboard />} />
+        <Route path="/dashboard/parent" element={<ParentDashboard />} />
         
         {/* Student Specific Routes */}
         <Route path="/dashboard/student/profile" element={<StudentProfile />} />
@@ -49,7 +60,6 @@ function App() {
         <Route path="/dashboard/student/settings" element={<StudentSettings />} />
 
         {/* Teacher Routes */}
-        {/* <Route path="/dashboard/teacher" element={<TeacherDashboard />} /> */}
         <Route path="/dashboard/teacher/profile" element={<TeacherProfile />} />
         <Route path="/dashboard/teacher/students" element={<TeacherStudents />} />
         <Route path="/dashboard/teacher/subjects" element={<TeacherSubjects />} />
@@ -58,6 +68,15 @@ function App() {
         <Route path="/dashboard/teacher/messages" element={<TeacherMessages />} />
         <Route path="/dashboard/teacher/statistics" element={<TeacherStatistics />} />
         <Route path="/dashboard/teacher/settings" element={<TeacherSettings />} />
+
+        {/* Parent Routes */}
+        <Route path="/dashboard/parent/children" element={<ParentChildren />} />
+        <Route path="/dashboard/parent/grades" element={<ParentGrades />} />
+        <Route path="/dashboard/parent/lessons" element={<ParentLessons />} />
+        <Route path="/dashboard/parent/teachers" element={<ParentTeachers />} />
+        <Route path="/dashboard/parent/messages" element={<ParentMessages />} />
+        <Route path="/dashboard/parent/payments" element={<ParentPayments />} />
+        <Route path="/dashboard/parent/settings" element={<ParentSettings />} />
       </Routes>
     </BrowserRouter>
   );
